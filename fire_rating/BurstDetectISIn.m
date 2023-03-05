1f6c167fecfe8b0aca80e4e014eed569afbbcd85
Yundid <512862613@qq.com>
2023-03-05 18:27:18 +0800

    添加网络爆发检测参数.

--- fire_rating/BurstDetectISIn.m
+++ fire_rating/BurstDetectISIn.m
@@ -1,8 +1,8 @@
-function [Burst, SpikeBurstNumber] = BurstDetectISIn( Spike, N, ISI_N )
+function [NBurst_Parameters, SpikeBurstNumber] = BurstDetectISIn( Spike, N, ISI_N )
     % 当连续的 N 个神经元的时间间隔小于等于该值 ISI_N 时，这些神经元就被认为是处于突发状态。
     
-    fprintf('Beginning burst detection.\n');
-    % Find when the ISI_N burst condition is met
+    fprintf('Beginning NBurst_Parameters detection.\n');
+    % Find when the ISI_N NBurst_Parameters condition is met
     % Look both directions from each spike
     dT = zeros(N,length(Spike))+inf;
     for j = 0:N-1
@@ -13,23 +13,23 @@ function [Burst, SpikeBurstNumber] = BurstDetectISIn( Spike, N, ISI_N )
     Criteria( min(dT)<=ISI_N ) = 1; % Spike passes condition if it is
     % included in a set of N spikes
     % with ISI_N <= threshold.
-    % %% Assign burst numbers to each spike
+    % %% Assign NBurst_Parameters numbers to each spike
     SpikeBurstNumber = zeros(size(Spike)) - 1; % Initialize to '-1'
-    INBURST = 0; % In a burst (1) or not (0)
-    NUM_ = 0; % Burst Number iterator
-    NUMBER = -1; % Burst Number assigned
-    BL = 0; % Burst Length
+    INBURST = 0; % In a NBurst_Parameters (1) or not (0)
+    NUM_ = 0; % NBurst_Parameters Number iterator
+    NUMBER = -1; % NBurst_Parameters Number assigned
+    BL = 0; % NBurst_Parameters Length
     for i = N:length(Spike)
-        if INBURST == 0 % Was not in burst.
-            if Criteria(i) % Criteria met, now in new burst.
+        if INBURST == 0 % Was not in NBurst_Parameters.
+            if Criteria(i) % Criteria met, now in new NBurst_Parameters.
                 INBURST = 1; % Update.
                 NUM_ = NUM_ + 1;
                 NUMBER = NUM_;
                 BL = 1;
-            else %Still not in burst, continue
+            else %Still not in NBurst_Parameters, continue
                 % continue %
             end
-        else % Was in burst.
+        else % Was in NBurst_Parameters.
             if ~ Criteria(i) % Criteria no longer met.
                 INBURST = 0; % Update.
                 if BL<N % Erase if not big enough.
@@ -41,38 +41,38 @@ function [Burst, SpikeBurstNumber] = BurstDetectISIn( Spike, N, ISI_N )
                 % This conditional statement is necessary to split apart
                 % consecutive bursts that are not interspersed by a tonic spike
                 % (i.e. Criteria == 0). Occasionally in this case, the second
-                % burst has fewer than 'N' spikes and is therefore deleted in
+                % NBurst_Parameters has fewer than 'N' spikes and is therefore deleted in
                 % the above conditional statement (i.e. 'if BL<N').
                 %
-                % Skip this if at the start of a new burst (i.e. 'BL>=N'
+                % Skip this if at the start of a new NBurst_Parameters (i.e. 'BL>=N'
                 % requirement).
                 %
-                NUM_ = NUM_ + 1; % New burst, update number.
+                NUM_ = NUM_ + 1; % New NBurst_Parameters, update number.
                 NUMBER = NUM_;
-                BL = 1; % Reset burst length.
+                BL = 1; % Reset NBurst_Parameters length.
             else % Criteria still met.
-                BL = BL + 1; % Update burst length.
+                BL = BL + 1; % Update NBurst_Parameters length.
             end
         end
-        SpikeBurstNumber(i) = NUMBER; % Assign a burst number to
+        SpikeBurstNumber(i) = NUMBER; % Assign a NBurst_Parameters number to
         % each spike.
     end
-    % %% Assign Burst information
-    fprintf('Assigning Burst information.\n');
+    % %% Assign NBurst_Parameters information
+    fprintf('Assigning NBurst_Parameters information.\n');
     MaxBurstNumber = max(SpikeBurstNumber);
-    Burst.T_start = zeros(1,MaxBurstNumber); % Burst start time [sec]
-    Burst.T_end = zeros(1,MaxBurstNumber); % Burst end time [sec]
-    Burst.S = zeros(1,MaxBurstNumber); % Size (total spikes)
-    Burst.C = zeros(1,MaxBurstNumber); % Size (total channels)
+    NBurst_Parameters.T_start = zeros(1,MaxBurstNumber); % NBurst_Parameters start time [sec]
+    NBurst_Parameters.T_end = zeros(1,MaxBurstNumber); % NBurst_Parameters end time [sec]
+    NBurst_Parameters.S = zeros(1,MaxBurstNumber); % Size (total spikes)
+    NBurst_Parameters.C = zeros(1,MaxBurstNumber); % Size (total channels)
     for i = 1:MaxBurstNumber
         ID = find( SpikeBurstNumber==i );
-        Burst.T_start(i) = Spike(ID(1));
-        Burst.T_end(i) = Spike(ID(end));
-        Burst.S(i) = length(ID);
+        NBurst_Parameters.T_start(i) = Spike(ID(1));
+        NBurst_Parameters.T_end(i) = Spike(ID(end));
+        NBurst_Parameters.S(i) = length(ID);
         if isfield( Spike, 'C' )
-            Burst.C(i) = length( unique(Spike.C(ID)) );
+            NBurst_Parameters.C(i) = length( unique(Spike.C(ID)) );
         end
     end
-    fprintf('Finished burst detection using %0.2f minutes of spike data.\n', ...
+    fprintf('Finished NBurst_Parameters detection using %0.2f minutes of spike data.\n', ...
         diff(Spike([1 end]))/60);
     end
